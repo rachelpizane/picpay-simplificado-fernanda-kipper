0e0e48159eecaaefb91fc691efb3e93b558a3b2d
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-26 18:36:09 -0300

    first commit

---
+++ picpaysimplicado/src/main/java/com/picpaysimplicado/PicpaysimplicadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplicado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplicadoApplication.class, args);
+	}
+
+}
---
+++ picpaysimplicado/src/test/java/com/picpaysimplicado/PicpaysimplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
